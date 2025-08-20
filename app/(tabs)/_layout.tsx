5714dd18888a903d8ca1e8a96973d8b761dacc58
dkdkwl <dkdkwl@naver.com>
2025-08-20 13:14:59 +0900

    당근처럼 한번 앱 만들어보기 테스트

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,11 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform,TouchableOpacity } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { MaterialIcons, MaterialCommunityIcons, Entypo, Ionicons, FontAwesome5 } from '@expo/vector-icons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -16,30 +15,55 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
+            height: 80,
           },
-          default: {},
+          default: {
+            height: 80,
+          },
         }),
+        tabBarButton: (props) => (
+          <TouchableOpacity {...props} style={[props.style, { justifyContent: 'center' }]} />
+        ),
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title:"홈",
+          tabBarIcon: ({ color }) => <Entypo name="home" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title:"동네생활",
+          tabBarIcon: ({ color }) => <MaterialIcons name="article" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="explore2"
+        options={{
+          title:"동네지도",
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="map-marker" size={26} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="explore3"
+        options={{
+          title:"채팅",
+          tabBarIcon: ({ color }) => <Ionicons name="chatbubble-ellipses-sharp" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="explore4"
+        options={{
+          title:"나의당근",
+          tabBarIcon: ({ color }) => <FontAwesome5 name="user-alt" size={22} color={color} />,
         }}
       />
     </Tabs>
+    
   );
 }
