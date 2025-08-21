6ab625461deeb92589fb95cff8970403c44d9a73
dkdkwl <dkdkwl@naver.com>
2025-08-21 16:43:43 +0900

    뷰페이지 프로필 작업중 + 스와이퍼 설치

--- app/detail.jsx
+++ app/detail.jsx
@@ -1,17 +1,94 @@
-import { Image, Dimensions, StyleSheet, View } from "react-native";
-import React from 'react'
+import { View, Image, Dimensions, StyleSheet,Text } from "react-native";
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { Colors } from '@/constants/Colors';
 
 const { width } = Dimensions.get("window");
 
-
 export default function ViewScreen() {
+  const colorScheme = useColorScheme();
+  const textColor = Colors[colorScheme ?? 'light'].text;
   return (
-    <View>
-    <Image
-      source={require("@/assets/images/젖병소독기.webp")}
-      style={{ width: width, height: 300 }}
-      resizeMode="cover" // cover: 가득 채움, contain: 전체 보이게
-    />
-    </View>
+    <ThemedView>
+      <Image
+        source={require("@/assets/images/젖병소독기.webp")}
+        style={{ width: width, height: 300 }}
+        resizeMode="cover"
+      />
+      <View style={styles.profileWrap}>
+        <View style={styles.profileLeft}>
+          <View style={styles.profileBox}>
+            <Image
+              source={require("@/assets/images/img_none_user.png")}
+              style={
+                styles.profileImg
+              }
+              resizeMode="cover"
+            />
+          </View>
+          <View style={styles.profileTextBox}>
+            <ThemedText style={[styles.profileTitle,{ color: textColor }]}>부기</ThemedText>
+            <ThemedText style={[styles.profileText,{ color: textColor}]}>부평구 부평동</ThemedText>
+          </View>
+        </View>
+        <View style={styles.profileRight}>
+          <Text style={styles.profileTemp}>71.5°C</Text>
+          <Text style={styles.profileTempText}>매너온도</Text>
+        </View>
+      </View>
+    </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  profileWrap:{
+    flexDirection:"row",
+    justifyContent:"space-between",
+    alignItems:"center",
+    paddingRight: 12,
+    paddingTop:15,
+    paddingBottom:15,
+    paddingLeft: 12,
+    borderBottomWidth:1,
+    borderBottomColor:"#cccccc"
+  },
+  profileLeft:{
+    flexDirection:"row"
+  },
+  profileRight:{
+
+  },
+  profileTemp:{
+    fontSize:16,
+    color:"#f60",
+    fontWeight:"800"
+  },
+  profileTempText:{
+    fontSize:12,
+    textDecorationLine:"underline"
+  },
+  profileBox: {
+    width:45,
+    height:45,
+    borderRadius: 100,
+    borderWidth: 1,
+    borderColor: "#ededed",
+    overflow: "hidden", // 둥글게 잘리도록 추가
+  },
+  profileTextBox:{
+    paddingLeft: 12,
+  },
+  profileTitle:{
+    fontSize:18,
+    fontWeight:800,
+  },
+  profileText:{
+    fontSize:12
+  },
+  profileImg:{
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+  }
+});
